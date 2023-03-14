bd1914048432c7db7f0547f1d834853784b0ac44
chiranshu18 <122433953+chiranshu18@users.noreply.github.com>
2023-03-14 14:12:40 +0530

    feat: tech stack card component (#31)
    * feat: create tech stack component
    
    * chore: refactor done
    
    ---------
    
    Co-authored-by: chiranshu khandelwal <chiranshumain2gmail.com>

---
+++ src/components/techStackCard/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './TechStackCard.css';
+import { BiCodeBlock } from 'react-icons/bi';
+import PropTypes from 'prop-types';
+
+const TechStack = ({ techName }) => {
+  return (
+    <div className="all-tech-stack-cards">
+      <div className="tech-stack-card">
+        <div>
+          <BiCodeBlock className="tech-icon" />
+        </div>
+
+        <div>
+          <p className="tech-stack-name">{techName}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+TechStack.propTypes = {
+  techName: PropTypes.string.isRequired,
+};
+export default TechStack;
