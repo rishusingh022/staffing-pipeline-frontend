e4027adcbfd41a88814395aa37ab04c207f52542
Sukhacoder02 <62611006+Sukhacoder02@users.noreply.github.com>
2023-03-15 09:22:28 +0530

    feat: add BE integration to list all engagements in projects page (#40)
    * fix: update type of children of CardContainer to be JSX.Element[]
    
    * feat: add backend call to fetch all users
    
    * feat: passed error message to error page in navigate func
    
    * chore: show a error message in the page rendered along with status code
    
    * feat: add BE integration to list all users
    
    * test: add snapshot tests for PeoplePage
    
    * feat: add BE integration to list all engagements
    
    * feat: add utils to convert JS Date() into human readadble form
    
    * test: add testcases for formatDate util
    
    * chore: update project dependency - add moment
    
    * chore: add formatDate to render date in correct form
    
    * fix: add overflow scroll in card-container
    
    * chore: add tailwind grids instead of traditional flexbox
    
    * chore: use user-image from assets
    
    * chore: update snapshots
    
    * fix: add update flag -u in npm script for test
    
    * chore: add responsiveness to CardContainer using tailwind grids
    
    * chore: add height to UserCard and EngagementCard

--- src/pages/EngagementsPage/index.jsx
+++ src/pages/EngagementsPage/index.jsx
@@ -1,15 +1,74 @@
 import * as React from 'react';
 import './EngagementsPage.css';
 import { Header } from '../../components';
-import Footer from '../../components/Footer';
+import CardContainer from './../../components/CardContainer';
+import makeRequest from '../../utils/makeRequest';
+import { GET_ENGAGEMENT_DATA_URL } from '../../constants/apiEndpoints';
+import { useNavigate } from 'react-router-dom';
+import EngagementCard from './../../components/EngagementCard';
+import formatDate from './../../utils/dateTime';
 const EngagementsPage = () => {
-  return (
-    <div>
-      <Header hasNav={true} />
-      <h1>Engagements Page</h1>
-      <Footer />
-    </div>
-  );
+  const navigate = useNavigate();
+  const [projects, setProjects] = React.useState([]);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    makeRequest(GET_ENGAGEMENT_DATA_URL, {}, navigate)
+      .then(response => {
+        setProjects(response);
+      })
+      .catch(error => {
+        setError(error);
+      });
+  }, []);
+
+  if (error) {
+    return (
+      <div>
+        <Header hasNav={true} />
+        <h1>People Page</h1>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
+  if (projects) {
+    const projectCards = projects.map(project => {
+      return (
+        <EngagementCard
+          imageUrl={project.image}
+          altText={project.name}
+          key={project.engagementId}
+          name={project.name}
+          startDate={formatDate(project.startDate)}
+          status={project.status}
+          identityNumber={project.engagementId}
+          handleButtonClick={() => {}}
+        />
+      );
+    });
+
+    return (
+      <div className="engagements-page">
+        <div className="header-in-engagements">
+          <Header hasNav={true} />
+        </div>
+        <div className="container-in-engagements">
+          <CardContainer>{projectCards}</CardContainer>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="engagements-page">
+        <div className="header-in-engagements">
+          <Header hasNav={true} />
+        </div>
+        <div className="container-in-engagements">
+          <CardContainer>Loading....</CardContainer>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default EngagementsPage;
---
+++ src/utils/dateTime/index.js
@@ -0,0 +1,6 @@
+import moment from 'moment';
+
+const formatDate = date => {
+  return moment(date).format('YYYY-MM-DD');
+};
+export default formatDate;
