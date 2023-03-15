3f0a84629100807db9ef634c367cc56bf602915a
Sneha Lall <78560129+sneh258@users.noreply.github.com>
2023-03-15 11:46:27 +0530

    Feat/create/img component (#45)
    * feat:create image component
    
    * test:add snapshot test for image component
    
    * fix : fix tests naming convention
    
    * fix : fix snapshot test
    
    * fix: fix border-radius in CSS
    
    * test : add snapshot test
    
    * feat: add image overlay

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import './image.css';
 import PropTypes from 'prop-types';
 
-export default function Image({ imageUrl, altText }) {
-  return <img src={imageUrl} alt={altText} className="image-component" />;
+export default function Image({ imageUrl, altText, hasOverlay }) {
+  return (
+    <>
+      {hasOverlay ? (
+        <>
+          <div className="container">
+            <img src={imageUrl} alt={altText} className="overlay-image-component" />
+            <div className="overlay">+</div>
+          </div>
+        </>
+      ) : (
+        <img src={imageUrl} alt={altText} className="image-component" />
+      )}
+    </>
+  );
 }
 
 Image.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   altText: PropTypes.string.isRequired,
+  hasOverlay: PropTypes.bool,
 };
