65442dd3a3ea6406d681f1c9586465faa32270e3
Harsh Agarwal <57107237+harshagarwal08@users.noreply.github.com>
2023-03-20 20:43:39 +0530

    fix: change CSS rules(#92)

--- src/pages/AddEngagementPage/index.jsx
+++ src/pages/AddEngagementPage/index.jsx
@@ -89,7 +89,7 @@ export default function AddEngagementPage() {
                 />
                 {engagementImage && <p>File selected: {engagementImage.name}</p>}
               </div>
-              <div className="ml-4 my-4 flex flex-col gap-2 engagement-form-container">
+              <div className="ml-4 mb-4 flex flex-col gap-2 add-engagement-form-container">
                 <input
                   type="text"
                   placeholder="Project Name"
@@ -106,75 +106,72 @@ export default function AddEngagementPage() {
                     setChargeCode(e.target.value);
                   }}
                 />
-                <div className="ml-4 my-4 flex flex-col gap-2 add-engagement-form-container">
-                  <input type="text" placeholder="Project Name" className="input-style" />
-                  <div className="flex gap-2">
-                    <input
-                      type="text"
-                      placeholder="MM/DD/YYYY"
-                      className="input-style w-32"
-                      onChange={e => {
-                        setStartDate(e.target.value);
-                      }}
-                    />
-                    <p>:</p>
-                    <input
-                      type="text"
-                      placeholder="MM/DD/YYYY"
-                      className="input-style w-32"
-                      onChange={e => {
-                        setEndDate(e.target.value);
-                      }}
-                    />
-                  </div>
-                  <Dropdown
-                    dropdownName="Status"
-                    dropdownData={statusOptions}
-                    selectOption={option => {
-                      setSelectedStatus(option.toLowerCase());
+                <div className="flex gap-2">
+                  <input
+                    type="text"
+                    placeholder="MM/DD/YYYY"
+                    className="input-style w-32"
+                    onChange={e => {
+                      setStartDate(e.target.value);
                     }}
                   />
-                  <div className="flex justify-between w-36 text-gray-400 px-2 border-black border">
-                    <p>Tags</p>
-                    <p>+</p>
-                  </div>
-                  <div className="flex gap-2">
-                    <div className="rounded-xl bg-black text-white w-20 text-xs py-1 text-center">Front-End</div>
-                    <div className="rounded-xl bg-black text-white w-20 text-xs py-1 text-center">Back-End</div>
-                  </div>
+                  <p>:</p>
+                  <input
+                    type="text"
+                    placeholder="MM/DD/YYYY"
+                    className="input-style w-32"
+                    onChange={e => {
+                      setEndDate(e.target.value);
+                    }}
+                  />
                 </div>
-              </div>
-              <Button buttonText="Create" handleClick={handleCreateClick} />
-            </div>
-            <div className="mid-container grid grid-cols-2 gap-12">
-              <div className="flex w-1/2 gap-2 flex-col">
-                <div className="flex items-center justify-between">
-                  <p className="font-bold text-2xl">Team Members</p>
-                  <Dropdown dropdownName="All" dropdownData={[]} selectOption={() => {}} />
+                <Dropdown
+                  dropdownName="Status"
+                  dropdownData={statusOptions}
+                  selectOption={option => {
+                    setSelectedStatus(option.toLowerCase());
+                  }}
+                />
+                <div className="flex justify-between w-36 text-gray-400 px-2 border-black border">
+                  <p>Tags</p>
+                  <p>+</p>
                 </div>
-                <div className="people-card-container-add">
-                  <div className="user-card-add">
-                    <BiPlus size={24} />
-                  </div>
-                </div>
-              </div>
-              <div className="flex w-1/2 gap-2 flex-col">
-                <div className="flex items-center justify-between">
-                  <p className="font-bold text-2xl">Technology</p>
-                  <Dropdown dropdownName="All" dropdownData={[]} selectOption={() => {}} />
-                </div>
-                <div className="tech-card-container-add">
-                  <div className="tech-card-add">
-                    <p className="px-4 font-semibold text-gray-400 cursor-pointer">Add +</p>
-                  </div>
+                <div className="flex gap-2">
+                  <div className="rounded-xl bg-black text-white w-20 text-xs py-1 text-center">Front-End</div>
+                  <div className="rounded-xl bg-black text-white w-20 text-xs py-1 text-center">Back-End</div>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col gap-2 case-study-container-add-engagement">
-              <p className="font-bold">Case Studies & Knowledge Materials</p>
-              <Dropdown dropdownName="All" dropdownData={[]} selectOption={() => {}} />
-              <Button buttonText="Upload" />
+            <Button buttonText="Create" handleClick={handleCreateClick} />
+          </div>
+          <div className="mid-container grid grid-cols-2 gap-12">
+            <div className="flex w-1/2 gap-2 flex-col">
+              <div className="flex items-center justify-between">
+                <p className="font-bold text-2xl">Team Members</p>
+                <Dropdown dropdownName="All" dropdownData={[]} selectOption={() => {}} />
+              </div>
+              <div className="people-card-container-add">
+                <div className="user-card-add">
+                  <BiPlus size={24} />
+                </div>
+              </div>
             </div>
+            <div className="flex w-1/2 gap-2 flex-col">
+              <div className="flex items-center justify-between">
+                <p className="font-bold text-2xl">Technology</p>
+                <Dropdown dropdownName="All" dropdownData={[]} selectOption={() => {}} />
+              </div>
+              <div className="tech-card-container-add">
+                <div className="tech-card-add">
+                  <p className="px-4 font-semibold text-gray-400 cursor-pointer">Add +</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 case-study-container-add-engagement">
+            <p className="font-bold">Case Studies & Knowledge Materials</p>
+            <Dropdown dropdownName="All" dropdownData={[]} selectOption={() => {}} />
+            <Button buttonText="Upload" />
           </div>
         </div>
       </div>
