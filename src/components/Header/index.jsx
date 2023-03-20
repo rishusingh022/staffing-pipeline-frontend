3423cfe2779c891c93751ee0baf6d9fd7b4c27ec
Aganya Bajaj <78909536+Cosmoboi-i@users.noreply.github.com>
2023-03-20 15:41:30 +0530

    Feat/component/header (#86)
    * feat: add header component with css
    
    * feat: add snapshot test
    
    * feat: add header functionality with no navbar
    
    * feat: add tests for header without navbar
    
    * fix: snapshots
    
    * fix: shift from traditional css to tailwind
    
    * fix: shift from traditional css to tailwind
    
    * chore: moved theme colors to tailwind config file
    
    * fix: header snapshots
    
    * fix: destructure props in header component
    
    * fix: fixed McKinsey logo aspect ratio in the header component
    
    * fix: header import in login page and app
    
    * fix: header import in app
    
    * feat: add navigate function to header
    
    * fix: header imports in components index file
    
    * fix: header import in components index file
    
    * fix: header import in engagement details page
    
    * fix: Header import in Engagement page
    
    * fix: move navigate funciton from useEffect to onClick and use window location for active class
    
    * fix: remove Header from EngagementsPage
    
    * fix: remove header import in engagements page
    
    * fix: header import in people page
    
    * fix: remove header import from people page
    
    * fix: header import in home page
    
    * fix: remove default export from components index file
    
    * fix: header import in home page
    
    * fix: header folder case issue
    
    * fix: header folder case issue #2
    
    * fix: default import for header in components index
    
    * feat: create a barebones modal component
    
    * Merged main with feat/component/modal
    
    * merged main to feat/component/modal
    
    * feat: add search functionality to the modal
    
    * fix: conflicts
    
    * fix: conflicts
    
    * fix: footer conflicts
    
    * chore: write tests for Modal and SearchAndAdd component
    
    * fix: replace cdn with npm package for font awesome
    
    * feat: integrate SearchAndAdd component with backend
    
    * Merged main into feat/component/modal
    
    * fix: remove log statements from SearchAndAdd
    
    * fix: Modal test- describe label
    
    * fix: modal not reopening
    
    * merge mainin feat/component/modal
    
    * fix: linting
    
    * feat: add logout functionality in header
    
    ---------
    
    Co-authored-by: Promit-revar <promit.revar2211@gmail.com>

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,16 +3,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CASE_STUDIES_ROUTE, PROJECTS_ROUTE, UPLOAD_EXCELL_ROUTE, USERS_ROUTE } from '../../constants/routes';
 import logoImage from '../../assets/McK_Logo.png';
+import Button from '../Button';
 export default function Header({ hasNav }) {
   const navigate = useNavigate();
   const location = window.location.pathname;
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
 
   const activeClass = 'text-cyan border-b-[5px] border-b-cyan border-solid pt-0 pb-2.5 px-[5px]';
   const inactiveClass =
     'text-[white] hover:text-cyan hover:border-b-[5px] hover:border-b-cyan hover:border-solid pt-0 pb-[15px] hover:pb-2.5 px-[5px]';
 
   return (
-    <div className="font-light h-[120px] text-white flex justify-center bg-deepBlue header-container relative">
+    <div className="font-light h-[120px] text-white flex justify-center bg-deepBlue header-container relative items-center">
       <img src={logoImage} className="h-[70px] w-[155px] aspect-auto self-end pl-0 pt-0 pb-[5px] absolute left-44" />
       <div className="h-full  flex box-border">
         {hasNav && (
@@ -40,6 +46,13 @@ export default function Header({ hasNav }) {
           </div>
         )}
       </div>
+      {hasNav && (
+        <button
+          className="absolute right-[5%] border-[1px] py-1 px-3 hover:text-cyan hover:border-cyan"
+          onClick={handleLogout}>
+          Logout
+        </button>
+      )}
     </div>
   );
 }
--- src/components/SearchAndAdd/index.jsx
+++ src/components/SearchAndAdd/index.jsx
@@ -12,6 +12,10 @@ const SearchAndAdd = ({ setIsOpen, entity, navigate, setItem }) => {
   const [debouncedInput] = useDebounce(input, 1000);
 
   useEffect(() => {
+    setIsOpen(open);
+  }, [open]);
+
+  useEffect(() => {
     handleSearch(debouncedInput);
   }, [debouncedInput]);
 
