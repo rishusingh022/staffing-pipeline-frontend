fce0167abcbc41172f2595c5afd8a551a6c96879
swapnil_06 <60195863+rishusingh022@users.noreply.github.com>
2023-03-20 14:43:44 +0530

    feat : add create new user api integrations (#83)

--- src/pages/AddNewPeoplePage/index.jsx
+++ src/pages/AddNewPeoplePage/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import './AddNewPeoplePage.css';
 import Header from '../../components/Header';
@@ -5,34 +6,116 @@ import Image from '../../components/Image';
 import userImage from '../../assets/images/user-default.png';
 import Button from '../../components/Button';
 import Dropdown from '../../components/Dropdown';
+import Notification from '../../components/Notification';
+import makeRequest from '../../utils/makeRequest';
+const { useNavigate } = require('react-router-dom');
+import { CREATE_USER_DATA_URL } from '../../constants/apiEndpoints';
+
 function AddNewPeoplePage() {
+  const navigate = useNavigate();
   const [activeTab, setActiveTab] = React.useState('skills');
+  const [fmno, setFmno] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [position, setPosition] = React.useState('');
+  const [office, setOffice] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [phone, setPhone] = React.useState('');
+  const [handleNotification, setHandleNotification] = React.useState(false);
+  const handleAddNewUser = () => {
+    makeRequest(
+      CREATE_USER_DATA_URL,
+      {
+        data: {
+          name: name,
+          email: email,
+          fmno: fmno,
+          caseStudyIds: [],
+          skills: [],
+          role: position,
+          guild: null,
+          image: null,
+        },
+      },
+      navigate
+    )
+      .then(() => {
+        setHandleNotification(true);
+      })
+      .catch(error => {
+        console.log('Error while adding user', error);
+      });
+  };
   return (
     <div>
       <Header hasNav />
+      {handleNotification && (
+        <Notification
+          message="User successfully added"
+          handleClose={() => {
+            setHandleNotification(false);
+          }}
+          success
+        />
+      )}
       <div className="add-new-people-container">
         <div className="add-new-people-upper-body">
           <div className="add-new-people-upper-body-left">
             <div className="add-new-people-upper-body-left-left">
               <Image imageUrl={userImage} altText="user" />
-              <Button buttonText="Add New User" />
+              <Button handleClick={handleAddNewUser} buttonText="Add New User" />
             </div>
             <div className="add-new-people-upper-body-left-right">
               <div className="new-people-fmno">
-                <input type="text" placeholder="FMNO" />
+                <input
+                  type="text"
+                  placeholder="FMNO"
+                  onChange={e => {
+                    setFmno(e.target.value);
+                  }}
+                />
               </div>
               <div className="new-people-name">
-                <input type="text" placeholder="Name" />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  onChange={e => {
+                    setName(e.target.value);
+                  }}
+                />
               </div>
               <div className="new-people-position-office">
-                <Dropdown dropdownName="Position" dropdownData={['CEO', 'CTO', 'CFO']} />
-                <Dropdown dropdownName="Office" dropdownData={['Bangalore', 'Chennai', 'Hyderabad']} />
+                <Dropdown
+                  dropdownName="Position"
+                  dropdownData={['Junior Engineer', 'Engineer1', 'Engineer2']}
+                  selectOption={position => {
+                    setPosition(position.toLowerCase());
+                  }}
+                />
+                <Dropdown
+                  dropdownName="Office"
+                  dropdownData={['Bangalore', 'Gurgaon']}
+                  selectOption={office => {
+                    setOffice(office.toLowerCase());
+                  }}
+                />
               </div>
               <div className="new-people-email">
-                <input type="text" placeholder="Email" />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  onChange={e => {
+                    setEmail(e.target.value);
+                  }}
+                />
               </div>
               <div className="new-people-phone">
-                <input type="text" placeholder="Phone" />
+                <input
+                  type="text"
+                  placeholder="Phone"
+                  onChange={e => {
+                    setPhone(e.target.value);
+                  }}
+                />
               </div>
             </div>
           </div>
