30b65189f222661bf0a7467ca71bdce80e59f907
Sukhacoder02 <62611006+Sukhacoder02@users.noreply.github.com>
2023-03-22 09:54:36 +0530

    fix: add navigate to login when jwt expired or when 401 unauthorised  (#109)
    * fix: add navigate to login when jwt expired or 401 status
    
    * chore: update snapshots

--- src/utils/makeRequest/index.js
+++ src/utils/makeRequest/index.js
@@ -18,11 +18,16 @@ const makeRequest = async (apiEndPoint, dynamicConfig = {}, navigate) => {
   } catch (e) {
     const errorStatus = e.response?.status;
     if (errorStatus) {
-      navigate(`${ERROR_ROUTE}/${errorStatus}`, {
-        state: {
-          message: e.response.data.error,
-        },
-      });
+      const message = e.response.data.error;
+      if ((message && message === 'jwt expired') || errorStatus === 401) {
+        navigate('/login');
+      } else {
+        navigate(`${ERROR_ROUTE}/${errorStatus}`, {
+          state: {
+            message: e.response.data.error,
+          },
+        });
+      }
     } else {
       navigate(ERROR_ROUTE);
     }
