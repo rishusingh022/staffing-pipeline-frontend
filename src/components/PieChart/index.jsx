fbe49f1c15419fd199801b628340aff01897ce8c
chiranshu18 <122433953+chiranshu18@users.noreply.github.com>
2023-03-31 00:57:48 +0530

    Feat: download charts functionality (#163)
    * feat: pie chart component
    
    * fix: pie chart title
    
    * feat: download functionality for charts
    
    * fix: eslint issues & minor changes
    
    ---------
    
    Co-authored-by: chiranshu khandelwal <chiranshumain2gmail.com>
    Co-authored-by: swapnil_06 <60195863+rishusingh022@users.noreply.github.com>

--- src/components/PieChart/index.jsx
+++ src/components/PieChart/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useRef } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import HC_more from 'highcharts/highcharts-more';
 HC_more(Highcharts);
 
+import exportingModule from 'highcharts/modules/exporting';
+exportingModule(Highcharts);
+
 // eslint-disable-next-line react/prop-types
 const PieChart = ({ data, name }) => {
+  const chartComponent = useRef(null);
+  // const printChartAsImage = () => {
+  //   // chart to image
+  //   const chart = chartComponent.current.chart;
+  //   console.log(chart.getSVG());
+  // };
+
   const options = {
     chart: {
       type: 'pie',
@@ -26,7 +37,10 @@ const PieChart = ({ data, name }) => {
 
   return (
     <div>
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      {/* <button onClick={printChartAsImage}>Download</button> */}
+      <div id="element-to-print">
+        <HighchartsReact highcharts={Highcharts} options={options} ref={chartComponent} />
+      </div>
     </div>
   );
 };
